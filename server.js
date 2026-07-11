4656cb2b58631c0b926fb972d999e2e48e8adba5
Manan Yadav <mananyadav1212@gmail.com>
2026-07-11 19:19:45 +0530

    Admin changes

--- server.js
+++ server.js
@@ -234,6 +234,6 @@ const sendWelcomeEmail = async (customerEmail, customerName) => {
 // =====================================================================
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Sakhi.co running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Sakhi.co running on port ${PORT}`);
 });
