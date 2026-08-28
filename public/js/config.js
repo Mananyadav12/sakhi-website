ed466fc8cf6fd41e156adfb13bd371e320aa4431
Mananyadav12 <mananyadav1212@gmail.com>
2026-08-28 09:10:05 +0530

    remove node_modules from git tracking

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
 // Yahan baad mein Render ka URL aayega
 // Abhi local testing ke liye ye hai
-const API_URL = 'https://sakhi-website.onrender.com';
+const API_URL = 'https://sakhi-on.onrender.com';
 const RAZORPAY_KEY = 'rzp_test_placeholder'; // Baad mein real key daalna
